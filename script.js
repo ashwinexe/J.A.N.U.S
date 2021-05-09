82021b3c4fabda4677f4043ad638f833956d6db8
ashwinexe <53075480+ashwinexe@users.noreply.github.com>
2021-05-09 19:19:17 +0530

    Track application page

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ document.addEventListener('DOMContentLoaded', function() {
     
     document.getElementById('cog').addEventListener("click", show_settings);
     document.getElementById('back').addEventListener("click", hide_settings);
+    document.getElementById('track').addEventListener("click", show_preview);
 })
-
-
-var a;
 
 function show_settings() {
         document.getElementById("settings_page").style.display="block";
         document.getElementById("index").style.display="none";
-}
-
-function hide_settings() {
-    document.getElementById("index").style.display="inline";
-    document.getElementById("settings_page").style.display="none";
-
+    }
+    
+    function hide_settings() {
+        document.getElementById("index").style.display="inline";
+        document.getElementById("settings_page").style.display="none";
+        
+    }
+    
+    function show_preview() {
+        document.getElementById("data_preview").style.display="block";
+        document.getElementById("index").style.display="none";
 }
