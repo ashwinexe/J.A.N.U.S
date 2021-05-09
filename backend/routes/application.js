c1450733953799bd41eb068c2023b23734d89872
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2021-05-09 21:03:49 +0530

    feat: delete application by id

--- backend/routes/application.js
+++ backend/routes/application.js
@@ -46,4 +46,18 @@ router.post('/', (req, res, next) => {
   });
 })
 
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+  connection.then(async (conn) => {
+    try {
+      const result = await conn.getRepository('applications').delete(id);
+
+      res.send(result);
+    } catch(exception) {
+      next(exception);
+    }
+  });
+})
+
 module.exports = router;
